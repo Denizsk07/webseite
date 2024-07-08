367c8cff99df5890752ebd90f0062957673c4985
lifee <koch@infaserv.de>
2024-07-08 18:00:49 +0200

    chore: Update metadata for open graph and Twitter cards
    This commit updates the metadata in the `layout.tsx` file to include open graph and Twitter card information. It adds the necessary `openGraph` and `twitter` objects to the `metadata` object, providing title, description, URL, type, and image information for both open graph and Twitter cards.
    
    This change ensures that when the website is shared on social media platforms, the correct title, description, and image are displayed, improving the overall appearance and user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,31 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Deniz-Kaya.de - Mediengestaltung in Bild und Ton",
   description: "Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten.",
+  openGraph: {
+    title: "Deniz-Kaya.de - Mediengestaltung in Bild und Ton",
+    description: "Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten.",
+    url: "https://deniz-kaya.de",
+    type: "website",
+    images: [
+      {
+        url: "favicon.ico",
+        width: 800,
+        height: 600,
+        alt: "Deniz Kaya",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Deniz-Kaya.de - Mediengestaltung in Bild und Ton",
+    description: "Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten.",
+    images: [
+      {
+        url: "favicon.ico",
+        alt: "Deniz Kaya",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +42,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten." />
+        <meta property="og:title" content="Deniz-Kaya.de - Mediengestaltung in Bild und Ton" />
+        <meta property="og:description" content="Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten." />
+        <meta property="og:image" content="favicon.ico" />
+        <meta property="og:url" content="https://deniz-kaya.de" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Deniz-Kaya.de - Mediengestaltung in Bild und Ton" />
+        <meta name="twitter:description" content="Erkunden Sie die Welt der Mediengestaltung in Bild und Ton mit Deniz Kaya und seinen Projekten." />
+        <meta name="twitter:image" content="favicon.ico" />
+        <title>Deniz-Kaya.de - Mediengestaltung in Bild und Ton</title>
+      </head>
       <body className={`${inter.className} min-h-screen relative`}>
         <Background />
         <div className="relative z-10 min-h-screen flex flex-col">
