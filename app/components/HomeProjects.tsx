817638d076cd013c95bea272eb4ec1260ee04051
Denizsk07 <deniz20070206@gmail.com>
2024-07-09 02:59:35 +0200

    Update HomeProjects.tsx

--- app/components/HomeProjects.tsx
+++ app/components/HomeProjects.tsx
@@ -21,7 +21,7 @@ export default function HomeProjects() {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="mt-20 mb-20 container mx-auto p-4">
       <h2 className="text-3xl font-bold text-center text-white mb-6">Meine Projekte</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {projects.map((project) => (
